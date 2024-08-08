09ca991f2719a254120bf11e147b47eeb916208b
Jhonatan Silva <jhonatan.jsilvajos@gmail.com>
2024-08-08 20:38:09 -0300

    Configuração Banco digital
    Signed-off-by: Jhonatan Silva <jhonatan.jsilvajos@gmail.com>

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# Banco-Digital---API-REST
+Projeto de banco digital que será realizado com as tecnologias Java, Spring Boot JPA, Hibernate e Banco de dados SQL.
---
+++ src/main/java/br/com/bancodigital/BdSoftsellApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdSoftsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdSoftsellApplication.class, args);
+	}
+
+}
